8da1d24d366655a9ecc08da9cf5760f868d87298
LuoAIStudio <GitHub>
2026-08-12 23:52:19 +0800

    完成Day03 Python基础学习

---
+++ Day03-Python-Git/Day3.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("This is Day 3 of the coding challenge.")#这是一个单行注释
+"""
+这是一个多行注释
+"""
+print(9//2)#这是去除小数点的整数
+print(9%2)#这是取余数
+print(9**4)#这是幂运算
+print(10-3)
+print(9/2)#这是除法运算
+print("passion"*5)#这是字符串重复
+print("(1+3)*3")
